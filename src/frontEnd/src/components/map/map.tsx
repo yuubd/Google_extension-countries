d6c733babac09d203af015065cf36ace03f1444f
yuubd <gc.kim93@gmail.com>
2020-03-28 14:23:47 -0700

    refactored `popup` to `frontEnd`

---
+++ src/frontEnd/src/components/map/map.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './map.css';
+
+function Map() {
+  return (
+    <div className="map">
+      map will be inserted here.
+    </div>
+  );
+}
+
+export default Map;
---
+++ src/frontEnd/src/components/wrapper/wrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './wrapper.css';
+
+function Wrapper() {
+  return (
+    <div className="wrapper">
+    </div>
+  );
+}
+
+export default Wrapper;
