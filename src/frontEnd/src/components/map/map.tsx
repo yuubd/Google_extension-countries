0c784fd3d157b8b310e2f10af0f72116e7e53856
yuubd <gc.kim93@hotmail.com>
2020-03-28 19:47:39 -0700

    Added Google Map

--- src/frontEnd/src/components/map/map.tsx
+++ src/frontEnd/src/components/map/map.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './map.css';
 
+import GoogleMapReact from 'google-map-react';
+
+
+const EXAMPLE_CENTER = {
+  center: {
+    lat: 59.95,
+    lng: 30.33
+  },
+  zoom: 11  
+};
+
 function Map() {
   return (
     <div className="map">
-      map will be inserted here.
+      <iframe title="test" frameBorder="0" 
+      src="https://www.google.com/maps/embed/v1/place?q=canada&key=KEY_GOES_HERE"/>
     </div>
   );
 }
