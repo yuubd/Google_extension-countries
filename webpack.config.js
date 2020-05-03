21e71f6b982da9be5ea0f7ec2a5df6e48b56e384
david.kim <david.kim@visier.com>
2020-05-03 13:54:18 -0700

    deleted files and changed settings

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
   mode: process.env.NODE_ENV,
   devtool: 'inline-source-map',
   entry: {
-    contentscript: join(__dirname, 'src/contentscript/contentscript.ts'),
-    background: join(__dirname, 'src/background/background.ts'),
+    contentscript: join(__dirname, 'src/contentscript.js'),
+    background: join(__dirname, 'src/background.js'),
   },
   output: {
     path: join(__dirname, 'dist'),
