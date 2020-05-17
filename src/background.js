0566183f862dc992174a1d33ba67c02b86ca1205
david.kim <david.kim@visier.com>
2020-05-16 23:33:32 -0700

    Modified to revent pushing multiple notifications

--- src/background.js
+++ src/background.js
@@ -1,5 +1,25 @@
+// push a notification when receives notification from contentscript
 chrome.runtime.onMessage.addListener(data => {
-    if (data.type === 'notification') {
-        chrome.notifications.create('', data.options);
+    if (data.type === "notification") {
+        chrome.notifications.create("", data.options);
     }
+});
+
+// send a msg to contentscript when a tab is created
+chrome.tabs.onUpdated.addListener(function(activeTab, createInfo) {
+    chrome.storage.local.get("monitoringTabId", (storage) => {
+        const monitoringTabId = storage.monitoringTabId;
+        if (monitoringTabId === undefined) {
+            chrome.storage.local.set({monitoringTabId: activeTab});
+        }
+        else if (monitoringTabId !== activeTab) {
+            // somehow it is never reset to false
+            chrome.storage.local.set({monitoringTabId: activeTab});
+        }
+        chrome.tabs.sendMessage(activeTab, {activeTab: activeTab});
+    });
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    // alert("detached" + tabId.id); 
 });
--- src/contentscript.js
+++ src/contentscript.js
@@ -1,68 +1,59 @@
-import { getRandomAlphaCode } from './frontEnd/src/layouts/mainLayout/utils';
+import { getRandomAlphaCode } from "./frontEnd/src/layouts/mainLayout/utils";
 
 
 // Listening to messages
-chrome.runtime.onMessage.addListener((msg, sender,senderRes) => {
-    console.log("conetnt Script received : " + msg);
-    
+chrome.runtime.onMessage.addListener((msg, sender, senderRes) => {
+    activityWatcher(msg.activeTab);
 });
 
-async function activityWatcher(){
-    chrome.storage.local.get("alphaCode", shouldContinue);
-    function shouldContinue(data) {
-        let alphaCode = data.alphaCode;
-        console.log("alphaCode is in content is : " + alphaCode);
-        if (typeof alphaCode != "undefined") { chrome.storage.local.clear(); return; }
-        //The number of seconds that have passed
-        //since the user was active.
-        let secondsSinceLastActivity = 0;
-
-        //Five minutes. 60 x 5 = 300 seconds.
-        let maxInactivity = 10;
-
-        //Setup the setInterval method to run
-        //every second. 1000 milliseconds = 1 second.
-        let monitoring = setInterval(setAlphaCode, 1000);        
-        function setAlphaCode(){
-            secondsSinceLastActivity++;
-            console.log("secondsSinceLastActivity" + secondsSinceLastActivity);
-            //if the user has been inactive or idle for longer
-            //then the seconds specified in maxInactivity
-            if(secondsSinceLastActivity > maxInactivity) {
-                const alphaCode = getRandomAlphaCode();
-                chrome.runtime.sendMessage('', {
-                    type: 'notification',
-                    options: {
-                        title: 'EduGlobe',
-                        message: 'Can you recognize this country? \nOpen Eduglobe it to explore!',
-                        iconUrl: `https://restcountries.eu/data/${alphaCode}.svg`,
-                        type: 'basic'
-                    } 
-                });
-                chrome.storage.local.set({alphaCode: alphaCode});
-                console.log('User has been inactive for more than ' + maxInactivity + ' seconds.');
-                maxInactivity = 3600;
-                clearInterval(monitoring);
-            }
-        }
-        //The function that will be called whenever a user is active
-        function reset(){
-            //reset the secondsSinceLastActivity letiable
-            //back to 0
-            secondsSinceLastActivity = 0;
-        }
-
-        //An array of DOM events that should be interpreted as user activity.
-        let activityEvents = [
-            'mousedown', 'mousemove', 'keydown',
-            'scroll', 'touchstart'
-        ];
-
-        //add these events to the document. register the activity function as the listener parameter.
-        activityEvents.forEach(function(eventName) {
-            document.addEventListener(eventName, reset, true);
-        });
+function activityWatcher(currentTab){
+    // An array of DOM events that should be interpreted as user activity and 
+    // add these events to the document. register the activity function as the listener parameter.
+    const activityEvents = ["mousedown", "mousemove", "keydown", "scroll", "touchstart"];
+    activityEvents.forEach((eventName) => {
+        document.addEventListener(eventName, reset, true);
+    });
+    // The function that will be called whenever a user is active
+    function reset() {
+        //reset the secondsSinceLastActivity letiable. back to 0
+        chrome.storage.local.set({monitoringTabId: currentTab});
+        secondsSinceLastActivity = 0;
     }
-}
 
-activityWatcher();
+    // The number of seconds that have passed since the user was active.
+    let secondsSinceLastActivity = 0;
+    
+    const maxInactivity = 6;
+    function monitor() {
+        let monitoring = setInterval(setAlphaCode, 10000);
+        function setAlphaCode() {
+            chrome.storage.local.get("monitoringTabId", (storage) => {
+                console.log("sorage.monitoringTabId : " + storage.monitoringTabId + "   currentTab : " + currentTab);
+                if (storage.monitoringTabId !== currentTab) { 
+                    return; 
+                } else {
+                    secondsSinceLastActivity++;
+                    console.log("secondsSinceLastActivity" + secondsSinceLastActivity);
+                    // if the user has been inactive or idle for longer then the seconds specified in maxInactivity
+                    if(secondsSinceLastActivity > maxInactivity) {
+                        const alphaCode = getRandomAlphaCode();
+                        chrome.runtime.sendMessage("", {
+                            type: "notification",
+                            options: {
+                                title: "EduGlobe",
+                                message: "Can you recognize this country? \nOpen Eduglobe it to explore!",
+                                iconUrl: `https://restcountries.eu/data/${alphaCode}.svg`,
+                                type: "basic"
+                            } 
+                        });
+                        console.log("Push a notification!");
+                        reset();
+                        chrome.storage.local.set({alphaCode: alphaCode});
+                    }
+                }
+            });
+        }
+    }
+    // main
+    monitor();
+}
