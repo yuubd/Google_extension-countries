6683b3782c6a5b213f6b3919cb8b469b263ab0ea
Jieun Lee <jieun-lee@users.noreply.github.com>
2020-05-18 20:34:48 -0700

    Add Icons to Button Bar

--- src/frontEnd/src/components/buttonBar/buttonBar.tsx
+++ src/frontEnd/src/components/buttonBar/buttonBar.tsx
@@ -6,8 +6,25 @@ function ButtonBar(props: { onClickPrev: Function, onClickNext: Function }) {
 
     return (
         <div className="button-bar">
-            <Button icon='left arrow' labelPosition='left' onClick={() => props.onClickPrev()} />
-            <Button icon='right arrow' labelPosition='right' onClick={() => props.onClickNext()} />
+            <div className="button-bar__section right-border">
+                <div className="button-bar__button">
+                    <i className="setting large icon" />
+                </div>
+                <div className="button-bar__button">
+                    <i className="bookmark outline large icon" />
+                </div>
+                <div className="button-bar__button">
+                    <i className="ellipsis horizontal large icon" />
+                </div>
+            </div>
+            <div className="button-bar__section left-border">
+                <div className="button-bar__button right-border" onClick={() => props.onClickPrev()}>
+                    <i className="reply large icon" />
+                </div>
+                <div className="button-bar__button left-border" onClick={() => props.onClickNext()}>
+                    <i className="share large icon" />
+                </div>
+            </div>
         </div>
     );
 }
