d506310774064dfa7fa09459964f27e32e31ba38
Jieun Lee <jieun-lee@users.noreply.github.com>
2020-05-22 17:16:15 -0700

    Make Settings Modal

--- src/frontEnd/src/layouts/settingsLayout/settingsLayout.tsx
+++ src/frontEnd/src/layouts/settingsLayout/settingsLayout.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './settingsLayout.css';
 
 import { PageHeader } from '../../components/pageHeader';
 import { SettingMenu } from '../../components/settingMenu';
+import { SettingModal } from '../../components/settingModal';
 
 function SettingsLayout(props: {setCurrPage: Function}) {
+
+    // states
+    const [modalType, setmodalType]: [string, any] = useState(""); // "" - modal closed
+    const [thresholdIdx, setThresholdIdx]: [number, any] = useState(3);
+    const [themeIdx, setThemeIdx]: [number, any] = useState(0);
+
+    // setting options
+    const thresholdArr = ["10 secs", "20 secs", "1 min", "2 mins"];
+    const themeArr = ["Light Mode", "Dark Mode"];
+
     return (
         <div className="settings-layout">
+            { (modalType === "threshold") &&
+                <SettingModal
+                    title="Time Threshold"
+                    options={thresholdArr}
+                    selected={thresholdArr[thresholdIdx]}
+                    onOptionChanged={(index: number) => setThresholdIdx(index)}
+                    onCloseClicked={() => setmodalType("")}
+                />
+            }
+            { (modalType === "theme") &&
+                <SettingModal
+                    title="Theme"
+                    options={themeArr}
+                    selected={themeArr[themeIdx]}
+                    onOptionChanged={(index: number) => setThemeIdx(index)}
+                    onCloseClicked={() => setmodalType("")}
+                />
+            }
             <PageHeader title="Settings" onGoBackClicked={()=> props.setCurrPage("")} />
             <div className="settings-layout__settings">
-                <SettingMenu title="Time Threshold" description="2 mins" onSettingClick={() => console.log("set threshold")} />
-                <SettingMenu title="Theme" description="Dark Mode" onSettingClick={() => console.log("set theme")} />
+                <SettingMenu title="Time Threshold" description={thresholdArr[thresholdIdx]} onSettingClick={() => setmodalType("threshold")} />
+                <SettingMenu title="Theme" description={themeArr[themeIdx]} onSettingClick={() => setmodalType("theme")} />
                 <SettingMenu title="Bookmarks" description="Re-visit your saved country cards" />
                 <SettingMenu title="Send Feedback" description="Tell us what you think" />
                 <SettingMenu title="App Version" description="00.00.00" />
