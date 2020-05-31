9f1b1dbb62a25a206c68cb9fd64c79cd02a27a75
Jieun Lee <jieun-lee@users.noreply.github.com>
2020-05-30 18:44:34 -0700

    Change Setting Button

--- src/frontEnd/src/mainLayout/components/settingButtonComponent/settingButtonModel.ts
+++ src/frontEnd/src/mainLayout/components/settingButtonComponent/settingButtonModel.ts
@@ -1,13 +1,20 @@
 export class SettingButtonModel {
     public readonly isSettingsPage: boolean;
     public readonly icon: string;
+    public readonly iconSrc: string;
 
     constructor(isSettingsPage: boolean = false) {
+
         function _getIcon(isSettingsPage: boolean): string {
-            return isSettingsPage ? "home" : "setting";
+            return isSettingsPage ? "home" : "settings";
+        }
+
+        function _getIconSrc(icon: string): string {
+            return require(`./assets/icon_${icon}.png`);
         }
 
         this.isSettingsPage = isSettingsPage;
         this.icon = _getIcon(isSettingsPage);
+        this.iconSrc = _getIconSrc(this.icon);
     }
 }
